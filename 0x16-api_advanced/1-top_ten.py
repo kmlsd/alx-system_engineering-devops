ab15c5b42d5a22526f458d998fe5cccbefe4b05e
kmlsd <kmlsd4757.com>
2024-06-20 15:40:56 -0700

    0x16-api_advance

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,10 +6,11 @@ import requests
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
     params = {
         "limit": 10
     }
-    response = requests.get(url, params=params,
+    response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code == 404:
         print("None")
