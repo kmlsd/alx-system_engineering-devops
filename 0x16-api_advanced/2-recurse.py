db92031e0fdd3c55a85124137cf44111fc67851f
kmlsd <kmlsd4757.com>
2024-06-20 15:43:35 -0700

    0x16-api_advance

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,23 +1,29 @@
-#!/usr/bin/python3
-""" Queries the Reddit API and returns a list
-containing the titles of all hot articles for a given subreddit. """
 
+#!/usr/bin/python3
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    url = "https://www.reddit.com/r/{}/hot.json?after={}"\
-          .format(subreddit, after)
-    request = requests.get(url, headers=headers, allow_redirects=False)
-
-    if request.status_code == 200:
-        for children in request.json().get("data").get("children"):
-            hot_list.append(children.get("data").get("title"))
-
-        after = request.json().get("data").get("after")
-        if not after:
-            return hot_list
-        return recurse(subreddit, hot_list, after)
-
-    else:
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
+
