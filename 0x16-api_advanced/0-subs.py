10ab5213a6d7c35782fd9fa9a273a68a5c38b737
kmlsd <kmlsd4757@gmail.com>
2024-08-13 12:46:10 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Queries the Reddit API and returns the
-number of subscribers for a given subreddit. """
+number of subscribers for a given subreddit. 
+"""
 
 import requests
 headers = {"User-Agent": "Custom"}
@@ -11,6 +12,6 @@ def number_of_subscribers(subreddit):
     request = requests.get(url, headers=headers, allow_redirects=False)
 
     if request.status_code == 200:
-        return request.json().get("data").get("subscribers")
+       return request.json().get("data").get("subscribers")
 
-    return 0
+    return (0)
