12f4f3d5994f610d22ac925a62dadf0f20df7f79
kmlsd <kmlsd4757@gmail.com>
2024-08-15 11:54:35 -0700

    subsicber

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,18 @@
 
 #!/usr/bin/python3
-"""
-    Uses Reddit API to print the number of subscribers of a subreddit
-"""
+""" Queries the Reddit API and returns the
+number of subscribers for a given subreddit. """
+
 import requests
+headers = {"User-Agent": "ubuntu"}
 
 
 def number_of_subscribers(subreddit):
-    """
-    Get the number of subscribers for a given subreddit
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'request'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    request = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
-        return 0
+    if request.status_code == 200:
+        return request.json().get("data").get("subscribers")
 
-    data = response.json().get("data")
-    num_subs = data.get("subscribers")
-
-    return num_subs
+    return 0
 
