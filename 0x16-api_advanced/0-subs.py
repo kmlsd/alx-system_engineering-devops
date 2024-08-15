4bb6f70f1458777eefce33ba240981e014969295
kmlsd <kmlsd4757@gmail.com>
2024-08-15 12:00:08 -0700

    subsicber

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,9 +10,9 @@ headers = {"User-Agent": "ubuntu"}
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     request = requests.get(url, headers=headers, allow_redirects=False)
-
+    
+    if response.status_code != 200:
+        return 0
     if request.status_code == 200:
         return request.json().get("data").get("subscribers")
-
-    return 0
 
